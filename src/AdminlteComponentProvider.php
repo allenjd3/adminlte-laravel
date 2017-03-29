a1c74f0cd7490eb53b1c44242e956a40437d4592
James Allen <jacques2186@yahoo.com>
2017-03-29 14:44:24 -0400

    added some things to provider to allow more control over installed files

--- src/AdminlteComponentProvider.php
+++ src/AdminlteComponentProvider.php
@@ -28,7 +28,52 @@ class AdminlteComponentProvider extends ServiceProvider
     	], 'partials');
     	$this->publishes([
         	__DIR__.'/views/partials/backend_left_sidebar.blade.php' => resource_path('/views/vendor/jd3_lte/partials/backend_left_sidebar.blade.php'),
-    	], 'left-sidebar');    	
+    	], 'left-sidebar');
+        $this->publishes([
+            __DIR__.'/views/partials/backend_content.blade.php' => resource_path('/views/vendor/jd3_lte/partials/backend_content.blade.php'),
+        ], 'content');   
+        $this->publishes([
+            __DIR__.'/views/partials/backend_footer.blade.php' => resource_path('/views/vendor/jd3_lte/partials/backend_footer.php'),
+        ], 'footer');
+        $this->publishes([
+            __DIR__.'/views/partials/backend_main_nav.blade.php' => resource_path('/views/vendor/jd3_lte/partials/backend_main_nav.blade.php'),
+        ], 'main-nav');   
+        $this->publishes([
+            __DIR__.'/views/partials/backend_scripts.blade.php' => resource_path('/views/vendor/jd3_lte/partials/backend_scripts.php'),
+        ], 'scripts');  
+        $this->publishes([
+            __DIR__.'/views/partials/backend_sidebar_left_searchform.blade.php' => resource_path('/views/vendor/jd3_lte/partials/backend_left_searchform.blade.php'),
+        ], 'searchform'); 
+        $this->publishes([
+            __DIR__.'/views/partials/backend_sidebar_right.blade.php' => resource_path('/views/vendor/jd3_lte/partials/backend_sidebar_right.php'),
+        ], 'right-sidebar');  
+        $this->publishes([
+            __DIR__.'/views/slots/button.blade.php' => resource_path('/views/vendor/jd3_lte/slots/button.blade.php'),
+        ], 'button');  
+        $this->publishes([
+            __DIR__.'/views/slots/callout.blade.php' => resource_path('/views/vendor/jd3_lte/slots/callout.blade.php'),
+        ], 'callout');   
+        $this->publishes([
+            __DIR__.'/views/slots/checkbox.blade.php' => resource_path('/views/vendor/jd3_lte/slots/checkbox.blade.php'),
+        ], 'checkbox');    
+        $this->publishes([
+            __DIR__.'/views/slots/ck_edit.blade.php' => resource_path('/views/vendor/jd3_lte/slots/ck_edit.blade.php'),
+        ], 'ck-edit');  
+        $this->publishes([
+            __DIR__.'/views/slots/content_box.blade.php' => resource_path('/views/vendor/jd3_lte/slots/content_box.blade.php'),
+        ], 'content-box');   
+        $this->publishes([
+            __DIR__.'/views/slots/form_component.blade.php' => resource_path('/views/vendor/jd3_lte/slots/form_component.blade.php'),
+        ], 'form-component'); 
+        $this->publishes([
+            __DIR__.'/views/slots/left_sidebar_basic_link.blade.php' => resource_path('/views/vendor/jd3_lte/slots/left_sidebar_basic_link.blade.php'),
+        ], 'link');
+        $this->publishes([
+            __DIR__.'/views/slots/left_sidebar_treeview_link.blade.php' => resource_path('/views/vendor/jd3_lte/slots/left_sidebar_treeview_link.blade.php'),
+        ], 'treeview');   
+        $this->publishes([
+            __DIR__.'/views/slots/left_sidebar_header.blade.php' => resource_path('/views/vendor/jd3_lte/slots/left_sidebar_header.blade.php'),
+        ], 'header-left');    	
     }
 
     /**
