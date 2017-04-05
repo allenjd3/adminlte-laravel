63a5dda20112a525b7464508ea04a44915042848
James Allen <jacques2186@yahoo.com>
2017-04-05 12:04:54 -0400

    Fixed the left sidebar. Added image control and user

--- src/views/partials/backend_left_sidebar.blade.php
+++ src/views/partials/backend_left_sidebar.blade.php
@@ -4,18 +4,20 @@
     <section class="sidebar">
 
       <!-- Sidebar user panel (optional) -->
+      @if(Auth::check())
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="@yield("image_uri","dist/img/user2-160x160.jpg")" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p>{{Auth::user()->name}}</p>
           <!-- Status -->
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
+      @endif
 
-      <!-- search form (Optional) -->
+      <!-- search form (Optional) 
       @include('jd3_lte::partials.backend_sidebar_left_searchform')
       <!-- /.search form -->
 
